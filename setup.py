dea6eaf0e4119e0767387157c303f00c61c6a324
yashing2 <argggggg@hmu.rip>
2024-11-30 08:59:35 +0100

    update v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nizzix",
-    version="1.5",
+    version="1.6",
     author="yaz_v2",
     author_email="discordyashing@gmail.com",
     url="https://github.com/yashing2/import-nizzix",
